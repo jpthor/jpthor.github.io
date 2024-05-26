8501ca7fae61aa394e466d2deaaa3840321f7c49
ehsan <ehsan.saradar@gmail.com>
2024-05-26 22:58:36 +0330

    Add json data source

--- main.js
+++ main.js
@@ -20,6 +20,73 @@ function loadFooter() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             document.getElementById('footer').innerHTML = xhr.responseText;
         }
+        loadJsonData();
     };
     xhr.send();
+}
+
+
+var jsonData = null;
+function loadJsonData() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', './site-data.json', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            jsonData = JSON.parse(xhr.responseText);
+            replaceInnerHTMLWithJSONValues();
+        }
+    };
+    xhr.send();
+
+}
+
+
+function getValue(query) {
+    const keys = query.split('.');
+    let index = null;
+    if (!isNaN(keys[keys.length - 1])) {
+        index = parseInt(keys.pop());
+    }
+    let value = jsonData;
+    for (const key of keys) {
+        if (value.hasOwnProperty(key)) {
+            value = value[key];
+        } else {
+            return null;
+        }
+    }
+    if (index !== null && Array.isArray(value)) {
+        if (index >= 0 && index < value.length) {
+            return value[index];
+        } else {
+            return null;
+        }
+    } else {
+        return value;
+    }
+}
+
+function replaceInnerHTMLWithJSONValues() {
+    const allElements = document.querySelectorAll('*');
+    allElements.forEach(element => {
+        const query = element.getAttribute('data-query');
+        if (query) {
+            // Set Alt values of images
+            if (element.getAttribute('data-attribute') === 'alt') {
+                const altValue = getValue(query);
+                element.setAttribute('alt', altValue);
+            } else {
+                // set innerhtml & href
+                const value = getValue(query);
+                if (value !== null && value !== undefined) {
+                    element.innerHTML = value;
+                }
+                if (element.getAttribute('data-attribute') === 'href') {
+                    const linkQuery = query.split('.').splice(0, query.split('.').length - 1).join('.').concat('.url');
+                    const hrefValue = getValue(linkQuery);
+                    element.setAttribute('href', hrefValue);
+                }
+            }
+        }
+    });
 }
