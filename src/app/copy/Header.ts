5dec2b9e0ec22b9eccd7b8dc8dd0960edd201dc5
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-07-09 13:55:18 +0330

    Update Header.ts

--- src/app/copy/Header.ts
+++ src/app/copy/Header.ts
@@ -1,7 +1,7 @@
 export const headerCopy = {
 
     title_1: "Learn,",
-    title_2: "Move,,",
+    title_2: "Move,",
     title_3: "& Create.",
     subtitle_1: "Accelerating Humanity's Potential through Determination and Self-belief.",
     subtitle_2: ""
